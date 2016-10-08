1414522cf716cfecf95daa9ff2580a0700f17111
CaoHong96 <caothihong.2710@gmail.com>
2016-10-08 13:22:31 +0700

    Sở thú
    hihi

---
+++ Sở thú SG/Sở thú SG/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Sở_thú_SG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstthumoi = new System.Windows.Forms.ListBox();
+            this.lstdanhsach = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblthoigian = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hồSơToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuload = new System.Windows.Forms.ToolStripMenuItem();
+            this.lưuDanhSáchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuclose = new System.Windows.Forms.ToolStripMenuItem();
+            this.sửaĐổiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mmucopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.cắtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dánToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstthumoi
+            // 
+            this.lstthumoi.AllowDrop = true;
+            this.lstthumoi.FormattingEnabled = true;
+            this.lstthumoi.Location = new System.Drawing.Point(23, 59);
+            this.lstthumoi.Name = "lstthumoi";
+            this.lstthumoi.Size = new System.Drawing.Size(130, 212);
+            this.lstthumoi.TabIndex = 0;
+            this.lstthumoi.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.lstthumoi.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
+            // 
+            // lstdanhsach
+            // 
+            this.lstdanhsach.AllowDrop = true;
+            this.lstdanhsach.FormattingEnabled = true;
+            this.lstdanhsach.Location = new System.Drawing.Point(170, 59);
+            this.lstdanhsach.Name = "lstdanhsach";
+            this.lstdanhsach.Size = new System.Drawing.Size(130, 212);
+            this.lstdanhsach.TabIndex = 1;
+            this.lstdanhsach.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstdanhsach_DragDrop);
+            this.lstdanhsach.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox_DragEnter);
+            this.lstdanhsach.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(20, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thú mới";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(167, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Danh sách thú";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.ImageIndex = 0;
+            this.button1.ImageList = this.imageList1;
+            this.button1.Location = new System.Drawing.Point(277, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Lưu danh sách";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolTip1.SetToolTip(this.button1, "\"Lưu danh sách thú mới vào danh sách thú\"");
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblthoigian
+            // 
+            this.lblthoigian.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblthoigian.AutoSize = true;
+            this.lblthoigian.Location = new System.Drawing.Point(23, 290);
+            this.lblthoigian.Name = "lblthoigian";
+            this.lblthoigian.Size = new System.Drawing.Size(35, 13);
+            this.lblthoigian.TabIndex = 5;
+            this.lblthoigian.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "save.png");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hồSơToolStripMenuItem,
+            this.sửaĐổiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(393, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hồSơToolStripMenuItem
+            // 
+            this.hồSơToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuload,
+            this.lưuDanhSáchToolStripMenuItem,
+            this.mnuclose});
+            this.hồSơToolStripMenuItem.Name = "hồSơToolStripMenuItem";
+            this.hồSơToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.hồSơToolStripMenuItem.Text = "Hồ sơ";
+            // 
+            // mnuload
+            // 
+            this.mnuload.Name = "mnuload";
+            this.mnuload.Size = new System.Drawing.Size(152, 22);
+            this.mnuload.Text = "Tải danh sách";
+            this.mnuload.Click += new System.EventHandler(this.mnuload_Click);
+            // 
+            // lưuDanhSáchToolStripMenuItem
+            // 
+            this.lưuDanhSáchToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("lưuDanhSáchToolStripMenuItem.Image")));
+            this.lưuDanhSáchToolStripMenuItem.Name = "lưuDanhSáchToolStripMenuItem";
+            this.lưuDanhSáchToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lưuDanhSáchToolStripMenuItem.Text = "Lưu danh sách";
+            // 
+            // mnuclose
+            // 
+            this.mnuclose.Name = "mnuclose";
+            this.mnuclose.Size = new System.Drawing.Size(152, 22);
+            this.mnuclose.Text = "Kết thúc";
+            this.mnuclose.Click += new System.EventHandler(this.mnuclose_Click);
+            // 
+            // sửaĐổiToolStripMenuItem
+            // 
+            this.sửaĐổiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mmucopy,
+            this.cắtToolStripMenuItem,
+            this.dánToolStripMenuItem});
+            this.sửaĐổiToolStripMenuItem.Name = "sửaĐổiToolStripMenuItem";
+            this.sửaĐổiToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.sửaĐổiToolStripMenuItem.Text = "Sửa đổi";
+            // 
+            // mmucopy
+            // 
+            this.mmucopy.Name = "mmucopy";
+            this.mmucopy.ShortcutKeyDisplayString = "Ctrl+C";
+            this.mmucopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.mmucopy.Size = new System.Drawing.Size(152, 22);
+            this.mmucopy.Text = "Sao";
+            // 
+            // cắtToolStripMenuItem
+            // 
+            this.cắtToolStripMenuItem.Name = "cắtToolStripMenuItem";
+            this.cắtToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+X";
+            this.cắtToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.cắtToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cắtToolStripMenuItem.Text = "Cắt";
+            // 
+            // dánToolStripMenuItem
+            // 
+            this.dánToolStripMenuItem.Name = "dánToolStripMenuItem";
+            this.dánToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+V";
+            this.dánToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.dánToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dánToolStripMenuItem.Text = "Dán";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 325);
+            this.Controls.Add(this.lblthoigian);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstdanhsach);
+            this.Controls.Add(this.lstthumoi);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstthumoi;
+        private System.Windows.Forms.ListBox lstdanhsach;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblthoigian;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hồSơToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuload;
+        private System.Windows.Forms.ToolStripMenuItem lưuDanhSáchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuclose;
+        private System.Windows.Forms.ToolStripMenuItem sửaĐổiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mmucopy;
+        private System.Windows.Forms.ToolStripMenuItem cắtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dánToolStripMenuItem;
+    }
+}
+
---
+++ Sở thú SG/Sở thú SG/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sở_thú_SG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuclose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblthoigian.Text = string.Format(" Bây giờ là {0}:{1}:{2} ngày {3} tháng {4} năm {5}",
+                DateTime.Now.Hour,
+                DateTime.Now.Minute,
+                DateTime.Now.Second,
+                DateTime.Now.Day,
+                DateTime.Now.Month,
+                DateTime.Now.Year);
+        }
+
+        private void mnuload_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("thumoi.txt");
+            if (reader == null)
+                return;
+            string input = null;
+            while ((input = reader.ReadLine()) != null)
+            {
+                lstthumoi.Items.Add(input);
+            }
+            reader.Close();
+            using (StreamReader rs = new StreamReader("danhsachthu.txt"))
+            {
+                input = null;
+                while ((input = rs.ReadLine()) != null)
+                    lstdanhsach.Items.Add(input);
+            }
+                
+        }
+        private void save(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("danhsachthu.txt");
+            if (writer == null) return;
+            foreach (var item in lstdanhsach.Items)
+                writer.WriteLine(item.ToString());
+            writer.Close();
+        }
+        private void ListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            int index = lb.IndexFromPoint(e.X, e.Y);
+            lb.DoDragDrop(lb.Items[index].ToString(), DragDropEffects.Copy);
+        }
+        private void ListBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void lstdanhsach_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                ListBox lb = (ListBox)sender;
+                lb.Items.Add(e.Data.GetData(DataFormats.Text));
+            }
+        }
+    }
+}
